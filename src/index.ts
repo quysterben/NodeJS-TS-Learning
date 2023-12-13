21fa3db041a1c4bf2e6e5959b1e1ab9e4aec54f5
Hoang Sy Quy <kiaisoft.quy.hoang@gmail.com>
2023-12-13 15:35:05 +0700

    feat: middlewares

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,9 @@ import helmet from 'helmet'
 
 import cookieParser from 'cookie-parser'
 
+import passport from 'passport'
+import { applyPassportStrategy } from './store/passport'
+
 import serverRoute from './routes/router'
 
 import db from './models'
@@ -21,9 +24,13 @@ app.use(express.urlencoded({ extended: false }))
 app.use(helmet())
 app.use(cors())
 
+// Passport
+applyPassportStrategy(passport)
+
 app.use(express.static('public'))
 app.use(cookieParser())
 
+// Multer
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'public/images')
@@ -32,7 +39,6 @@ const fileStorage = multer.diskStorage({
     cb(null, `${Date.now()}-${file.originalname}`)
   }
 })
-
 const fileFilter = (req: Request, file: any, cb: any) => {
   if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
     cb(null, true)
@@ -40,17 +46,14 @@ const fileFilter = (req: Request, file: any, cb: any) => {
     cb(null, false)
   }
 }
-
 app.use(multer({ storage: fileStorage, fileFilter }).array('image', 10))
-
 app.use('/res/images', express.static(path.join(__dirname, 'res', 'images')))
 
+// Routes
 app.get('/', (req: Request, res: Response) => {
   res.json({ messenge: 'Hello World' })
 })
-
 app.use('/api/v1', serverRoute)
-
 app.use((err: any, req: Request, res: Response, next: any) => {
   const status = err.statusCode || 500
   const message = err.message
@@ -62,6 +65,7 @@ app.use((err: any, req: Request, res: Response, next: any) => {
   })
 })
 
+// Connect Database
 db.mongoose
   .connect(
     `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@ecomshopcluster.evimone.mongodb.net/${process.env.DATABASE_NAME}`
